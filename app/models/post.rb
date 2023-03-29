60fbc6c8551aaf306efc2e3539a9106933a71c7c
+ <+>
2023-03-28 23:11:27 -0400

    commit tree 1dcd5dab614dc2e080e5416bded48bbe72a6738f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion:{ in: %w(Fiction Non-Fiction)}
+
+    validate :title_is_clickbait?
+
+    PHRASES = [
+        /Won't Believe/,
+        /Secret/,
+        /Top Number/,
+        /Guess/
+    ]
+
+    def title_is_clickbait?
+        if PHRASES.none? { |phrase| phrase.match(title) }
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
